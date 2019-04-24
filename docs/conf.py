735bbaa36a58b5db346efc66268cb6d740ddaaa5
joel kim <kim.dohhyoung@gmail.com>
2019-04-24 16:10:52 +0900

    add po files

--- docs/conf.py
+++ docs/conf.py
@@ -250,3 +250,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+locale_dirs = ['locale/']
+gettext_compact = False
